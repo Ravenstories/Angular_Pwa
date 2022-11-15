645d76b8b5c788dcf8a46170156fb1882055c5c6
Jacob Henriksen <Henriksen.Jacob@gmail.com>
2022-11-15 22:00:09 +0100

    git pages ?

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,8 +2,10 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { TableComponent } from './components/table/table.component';
 import { UploadImagesComponent } from './components/upload-images/upload-images.component';
+import { AppComponent } from './app.component';
 
 const routes: Routes = [
+  {path: "", component: AppComponent},
   {path: "table", component: TableComponent},
   {path: "upload", component: UploadImagesComponent}
 ];
